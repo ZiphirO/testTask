600cebdaae2ac8564ff61438d38b366d0c36dfe4
ziphiro <xgrachx@gmail.com>
2024-09-30 02:17:15 +0300

    рефактор fetchPersonsInfo()

--- src/main/java/com/example/testTask/service/RequestContentService.java
+++ src/main/java/com/example/testTask/service/RequestContentService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,20 +17,28 @@ public class RequestContentService {
 
     private static final ObjectMapper MAPPER = new ObjectMapper();
     private final RequestContentRepository REQUEST_CONTENT_REPOSITORY;
-
-    private final File person2 = new File("src/main/resources/data/persons/person2.json");
-    private final File person1 = new File("src/main/resources/data/persons/person1.json");
-    private final File person3 = new File("src/main/resources/data/persons/person3.json");
+    private final File PERSON_DIR = new File("src/main/resources/data/persons");
+//    private final File person2 = new File("src/main/resources/data/persons/person2.json");
+//    private final File person1 = new File("src/main/resources/data/persons/person1.json");
+//    private final File person3 = new File("src/main/resources/data/persons/person3.json");
 
     public RequestContent createRequestContent(RequestContent requestContent){
             return REQUEST_CONTENT_REPOSITORY.save(requestContent);
     }
 
     public List<RequestContent> fetchPersonsInfo() throws IOException{
-        RequestContent p1 = new RequestContent(MAPPER.readTree(person1));
-        RequestContent p2 = new RequestContent(MAPPER.readTree(person2));
-        RequestContent p3 = new RequestContent(MAPPER.readTree(person3));
-        List<RequestContent> pList = List.of(p1, p2, p3);
+        File[] personList = PERSON_DIR.listFiles();
+//        RequestContent p1 = new RequestContent(MAPPER.readTree(person1));
+//        RequestContent p2 = new RequestContent(MAPPER.readTree(person2));
+//        RequestContent p3 = new RequestContent(MAPPER.readTree(person3));
+        List<RequestContent> pList = new ArrayList<>();
+        assert personList != null;
+        for (File file : personList){
+            if (file.isFile()){
+                RequestContent pers = new RequestContent(MAPPER.readTree(file));
+                pList.add(pers);
+            }
+        }
         return REQUEST_CONTENT_REPOSITORY.saveAll(pList);
     }
 }
